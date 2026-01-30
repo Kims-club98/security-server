4ff5846f781f441c18b557cca3073a82f4dc21bf
Kims-club98 <naxen5611@naver.com>
2026-01-30 10:32:59 +0900

    자동배포 추가2

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "home page";
+        return "home page 2";
     }
 }
